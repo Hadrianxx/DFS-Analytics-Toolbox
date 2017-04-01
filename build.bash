0dbe3d1386f230f378ddbe7f67c60a7cdfbaad1d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-04-01 10:18:41 -0700

    use sudo for docker commands

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-export HOST_PROJECT_HOME=~/snarfblatt
-docker-compose -f build.yml up --build
+export HOST_PROJECT_HOME=~/dfs_projects
+sudo docker-compose -f build.yml up --build
--- run.bash
+++ run.bash
@@ -1,5 +1,4 @@
 #! /bin/bash
 
-export HOST_PROJECT_HOME=~/snarfblatt
-docker-compose down
-docker-compose up
+export HOST_PROJECT_HOME=~/dfs_projects
+sudo docker-compose up
