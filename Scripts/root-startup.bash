8881d59f3688c75b3b8935dd1f94a91d2716a2ed
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-04-01 21:38:51 -0700

    no more owner hack required!

--- Scripts/root-startup.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# Copyright (C) 2017 M. Edward (Ed) Borasky <znmeb@znmeb.net>
-# License: MIT
-
-echo "DFSTOOLS_HOME is $DFSTOOLS_HOME"
-chown -R dfstools:dfstools $DFSTOOLS_HOME
-ls -Altr $DFSTOOLS_HOME
-su dfstools -c '/Scripts/dfstools-startup.bash'
