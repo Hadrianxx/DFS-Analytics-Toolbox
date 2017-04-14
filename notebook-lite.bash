8634db36ba9ca63e6157eb83a1fcf999264ab474
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-04-14 14:55:04 -0700

    add clusters

--- notebook-lite.bash
+++ notebook-lite.bash
@@ -3,5 +3,9 @@
 export JULIA_TARBALL=https://julialang.s3.amazonaws.com/bin/linux/x64/0.5/julia-0.5.1-linux-x86_64.tar.gz \
   && curl -Ls $JULIA_TARBALL | sudo tar xfz - --strip-components=1 --directory=/usr/local \
   && julia -e 'Pkg.update(); Pkg.add("IJulia")'
-~/.julia/v0.5/Conda/deps/usr/bin/conda create --name jupyter python=3 --yes jupyter r-irkernel
+export CONDA_BINARIES=$HOME/.julia/v0.5/Conda/deps/usr/bin
+export PATH=$CONDA_BINARIES:$PATH
+conda create --name jupyter --yes python=3 jupyter r-irkernel ipyparallel
+source activate jupyter > /dev/null 2>&1
+ipcluster nbextension enable --user
 cat jupyter-aliases >> ~/.bashrc
