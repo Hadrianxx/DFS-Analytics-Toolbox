859fbf839b1067b0c55eb9ba4535b18eb486054f
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-04-16 15:35:09 -0700

    add devtools and tidyverse, drop ipyparallel

--- notebook-lite.bash
+++ notebook-lite.bash
@@ -5,7 +5,11 @@ export JULIA_TARBALL=https://julialang.s3.amazonaws.com/bin/linux/x64/0.5/julia-
   && julia -e 'Pkg.update(); Pkg.add("IJulia")'
 export CONDA_BINARIES=$HOME/.julia/v0.5/Conda/deps/usr/bin
 export PATH=$CONDA_BINARIES:$PATH
-conda create --name jupyter --yes python=3 jupyter r-irkernel ipyparallel
+conda create --name jupyter --yes python=3 \
+  jupyter \
+  r-devtools \
+  r-irkernel \
+  r-roxygen2 \
+  r-tidyverse
 source activate jupyter > /dev/null 2>&1
-ipcluster nbextension enable --user
 cat jupyter-aliases >> ~/.bashrc
