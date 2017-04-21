46c09528bba6075a6fbd351c2cd552c2d1cea59b
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-04-21 01:39:13 -0700

    next steps as comments

--- Windows/bootstrap.R
+++ Windows/bootstrap.R
@@ -6,3 +6,10 @@ install.URL("https://cran.rstudio.com/bin/windows/Rtools/Rtools34.exe")
 install.URL("https://github.com/git-for-windows/git/releases/download/v2.12.2.windows.2/Git-2.12.2.2-32-bit.exe")
 install.URL("https://s3.amazonaws.com/julialang/bin/winnt/x86/0.5/julia-0.5.1-win32.exe")
 install.URL("https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86.exe")
+# capture R path
+# capture Julia path
+# capture Jupyter path
+# capture Conda path
+# create environment
+# install R and Julia kernels
+# fire up a notebook!
