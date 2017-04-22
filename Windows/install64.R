de88b56c4426df456fe449f76390da552da38ad0
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-04-22 00:30:32 -0700

    installers for 64-bit machines

---
+++ Windows/install64.R
@@ -0,0 +1,15 @@
+install.packages(c("devtools", "installr", "stringr"))
+devtools::install_github("IRkernel/IRkernel")
+library(installr)
+install.RStudio()
+install.URL("https://cran.rstudio.com/bin/windows/Rtools/Rtools34.exe")
+install.URL("https://github.com/git-for-windows/git/releases/download/v2.12.2.windows.2/Git-2.12.2.2-64-bit.exe")
+install.URL("https://s3.amazonaws.com/julialang/bin/winnt/x64/0.5/julia-0.5.1-win64.exe")
+install.URL("https://repo.continuum.io/miniconda/Miniconda3-latest-Windows-x86_64.exe")
+# capture R path
+# capture Julia path
+# capture Jupyter path
+# capture Conda path
+# create environment
+# install R and Julia kernels
+# fire up a notebook!
