8be3cd709e6fb3c33bb8a6a939e1626c7bb8fbd9
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-04-26 16:02:36 -0700

    docker-compose removal

--- Docker/run.bash
+++ Docker/run.bash
@@ -1,4 +1,9 @@
-#! /bin/bash
+#! /bin/bash -v
 
 export HOST_PROJECT_HOME=~/dfs_project_home
-docker-compose up
+docker run -it --rm \
+  -u dfstools \
+  -p 8888:8888 \
+  -v $HOST_PROJECT_HOME:/home/dfstools/Projects \
+  docker.io/znmeb/dfstools:latest \
+  /Scripts/dfstools-startup.bash
