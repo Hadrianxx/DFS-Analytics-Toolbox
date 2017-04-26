48f4818db5f9ee7df2a3a47fb2b285ebe5a11a78
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2017-04-26 16:18:00 -0700

    not using any more!! huzzah!!

--- Docker/Scripts/root-startup.bash
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-# Copyright (C) 2017 M. Edward (Ed) Borasky <znmeb@znmeb.net>
-# License: MIT
-
-chown -R dfstools:dfstools /home/dfstools
-su dfstools -c /Scripts/dfstools-startup.bash
